706b63509d9ba997755a26b37bdc4379c0a1e338
antoine lieutaud <antoine.lieutaud@gmail.com>
2021-05-24 17:45:58 +0200

    first deployment

--- config/routes.rb
+++ config/routes.rb
@@ -3,16 +3,5 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :restaurants, only: [:new, :create] do
-    resources :offers, only: [:index, :new, :create]
-  end
-
-  resources :offers, only: [:update, :edit] do
-    resources :vouchers, only: [:new, :create]
-  end
-
-  resources :vouchers, only: [:show] do
-    resources :redeems, only: [:create]
-  end
-
+  resources :restaurants, only: [:new, :create]
 end
