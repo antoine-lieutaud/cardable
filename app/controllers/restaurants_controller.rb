82c820ad254410523fc15db6acafe226d7a5b385
antoine lieutaud <antoine.lieutaud@gmail.com>
2021-05-25 10:51:34 +0200

    after modif

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,17 @@
 class RestaurantsController < ApplicationController
+    # def new
+    #     @restaurant = Restaurant.new
+    #     authorize @restaurant
+    #   end
+    
+    #   def create
+    #     @restaurant = Restaurant.new(restaurant_params)
+    #     @restaurant.user = current_user
+    #     @travel.save
+    #     authorize @travel
+    #     redirect_to travels_path
+    #   end
+
+
+
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,5 @@
 class Restaurant < ApplicationRecord
-  belongs_to :owner, class_name: User, foreign_key: :user_id
+  belongs_to :owner, class_name: "User", foreign_key: :user_id
 
   has_many :staffs
   has_many :users, through: :staffs
