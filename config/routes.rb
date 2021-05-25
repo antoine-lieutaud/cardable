8b9c6f8c4e8aa7b510b8eb2c215e0e73a0449959
antoine lieutaud <antoine.lieutaud@gmail.com>
2021-05-25 11:21:25 +0200

    first seed

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :restaurants, only: [:new, :create]
+  resources :restaurants, only: [:new, :create, :show] do
+    resources :offers, only: [:index, :new, :create]
+  end
+
+
 end
