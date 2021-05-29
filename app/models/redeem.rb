c8347b7f0d3ae5e614d18c677cf89bbccb4dbb29
antoine lieutaud <antoine.lieutaud@gmail.com>
2021-05-29 04:41:28 +0200

    add consommation voucher

--- app/models/redeem.rb
+++ app/models/redeem.rb
@@ -1,3 +1,15 @@
 class Redeem < ApplicationRecord
   belongs_to :voucher
+  after_create :add_redeem
+
+
+
+  private
+
+  def add_redeem
+    self.voucher.nb_redeems += 1
+    self.voucher.save!
+  end
+
+
 end
