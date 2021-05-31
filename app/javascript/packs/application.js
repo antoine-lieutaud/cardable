64fc3720173d9c151b0c1064336118505b12aff4
Hugo Malfait <hugomalfait@outlook.fr>
2021-05-31 15:57:57 +0200

    messages scan

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -28,12 +28,33 @@ import "bootstrap";
 // Internal imports, e.g:
 import { initNavbar } from '../plugins/navbar';
 import { scanQR } from '../plugins/scan_qr';
+import { initSweetalert } from '../plugins/init_sweetalert';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   initNavbar();
   scanQR();
+
+  initSweetalert('#sweet-alert-demo', {
+    title: "Valide",
+    text: "Un menu, 2 personnes, genere le XX/XX/XXXX au nom de X \n il reste 2 utilisations",
+    icon: "success",
+    buttons: ["Annuler", "Utiliser le coupon"]
+  }, (value) => {
+    console.log(value);
+  });
+
+  initSweetalert('#sweet-alert-demo2', {
+    title: "Non Valide",
+    text: "A faire: if validate = true, return 'Ce coupon a deja ete utilise le XX/XX/XXXX' else 'ce coupon n'a jamais ete genere",
+    icon: "error",
+    buttons: "Retour"
+  }, (value) => {
+    console.log(value);
+  });
 });
 
 import "controllers"
+
+
--- app/javascript/plugins/scan_qr.js
+++ app/javascript/plugins/scan_qr.js
@@ -11,12 +11,16 @@ const scanQR = () => {
 const successCallback = (result) => {
   console.log("result", result)
   document.getElementById("console").innerText = result;
-  window.location.replace(result) 
+  window.location.replace(result);
+  const btnValide = document.getElementsById("sweet-alert-demo").click();
+  console.log(btnValide)
 }
 
 const failureCallback = (result) => {
   console.log("fail", result)
   document.getElementById("console").innerText = result;
+  const btnInvalide = document.getElementsById("sweet-alert-demo2").click();
+  console.log(btnInvalide)
 }
 
 export { scanQR };
